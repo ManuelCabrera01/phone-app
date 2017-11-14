0f5d815d00a02478232b4b8a82771aee79f1944e
Manuel <m.a.c.p90@gmail.com>
2017-11-13 20:38:27 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhoneListComponent } from './phone-list/phone-list.component';
+import { PhoneDetailsComponent } from './phone-details/phone-details.component';
+import { RouterModule } from "@angular/router";
+import { routes } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneListComponent,
+    PhoneDetailsComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
